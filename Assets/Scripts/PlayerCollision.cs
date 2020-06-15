33175393620da679c6867b4fe5402d2c1ecbba7d
WinchesterOP <neco.giedrojc@gmail.com>
2020-06-15 22:00:29 +0200

    Hindernisse und GameScore hinzugefügt

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("Player hit an obstacle");
+
+            playerMovement.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreNumber.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreNumber : MonoBehaviour
+{
+    public Transform player;
+    public Text textScore;
+
+    private float playerStartPosition;
+
+    private void Start()
+    {
+        playerStartPosition = player.position.z;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textScore.text = (player.position.z - playerStartPosition).ToString("0");
+    }
+}
